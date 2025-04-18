f6d2340277280387524523d7a4f0591990301643
Daniel Hensley <daniel@overzealouspelican.com>
2025-04-18 02:04:16 -0400

    fix: Update prod API URL

--- src/main/frontend/ng-food-inventory/src/environments/environment.prod.ts
+++ src/main/frontend/ng-food-inventory/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://inventory.lan.overzealouspelican.com/api",
+  apiUrl: "https://inventory.overzealouspelican.com/api",
   inventoryKey: "Default"
 };
