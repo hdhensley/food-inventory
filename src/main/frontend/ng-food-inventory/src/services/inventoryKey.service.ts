91cb8d38ee90e20e06e40a4dcb1b60de3c8cf651
Daniel Hensley <daniel@overzealouspelican.com>
2025-05-27 14:30:59 -0400

    docs: Added documentation to inventory key service

--- src/main/frontend/ng-food-inventory/src/services/inventoryKey.service.ts
+++ src/main/frontend/ng-food-inventory/src/services/inventoryKey.service.ts
@@ -13,11 +13,24 @@ import { Inventory } from 'src/models/Inventory.model';
   providedIn: 'root',
 })
 export class InventoryKeyService {
+  /**
+   * The current inventory key, which is stored in local storage.
+   * It defaults to the key defined in the environment configuration.
+   */
   public key: WritableSignal<string> = signal(environment.inventoryKey);
+  /**
+   * A set of all inventory keys that have been loaded or created.
+   * This is used to keep track of available inventory keys.
+   */
   public allKeys: WritableSignal<Set<string>> = signal(new Set());
 
   private http = inject(HttpClient);
 
+  /**
+   * Initializes the InventoryKeyService by loading the inventory key from local storage/environment
+   * and fetching all available inventory keys from the server.
+   * It also sets up an effect to update local storage whenever the key changes.
+   */
   constructor() {
     this.loadKey();
     this.getAllInventoryKeys();
@@ -25,15 +38,22 @@ export class InventoryKeyService {
     effect(() => localStorage.setItem('inventoryKey', this.key()));
   }
 
+  /**
+   * Loads the inventory key from local storage or uses the default key from the environment.
+   * It also updates the set of all keys with the loaded key.
+   */
   public loadKey(): void {
-    const savedKey =
-      localStorage.getItem('inventoryKey') || environment.inventoryKey;
+    const savedKey = localStorage.getItem('inventoryKey') || environment.inventoryKey;
     this.key.set(savedKey);
-    this.allKeys.update(curKeys => {
-      return new Set([...curKeys, savedKey]);
-    });
+    this.allKeys.update(curKeys => new Set([...curKeys, savedKey]));
   }
 
+  /**
+   * Creates a new inventory with the specified key.
+   * If the inventory already exists, it updates the set of all keys.
+   * 
+   * @param inventoryKey The key for the new inventory.
+   */
   public createInventory(inventoryKey: string): void {
     this.http
       .get<Inventory>(
@@ -55,15 +75,15 @@ export class InventoryKeyService {
       });
   }
 
+  /**
+   * Fetches all inventory keys from the server and updates the set of all keys.
+   * This method is called to populate the list of available inventory keys.
+   */
   public getAllInventoryKeys(): void {
     this.http
       .get<string[]>(`${environment.apiUrl}/inventoryKey`)
       .subscribe({
-        next: (keys: string[]) => {
-          this.allKeys.update(allKeys => {
-            return new Set([...allKeys, ...keys]);
-          });
-        },
+        next: (keys: string[]) => this.allKeys.update(allKeys => new Set([...allKeys, ...keys])),
         error: console.error,
       });
   }
