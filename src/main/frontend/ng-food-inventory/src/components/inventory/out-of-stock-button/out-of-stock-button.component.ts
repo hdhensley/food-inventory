8a27574a34c03935fb2711c4d6baff8e288361d7
Daniel Hensley <daniel@overzealouspelican.com>
2025-05-27 16:11:03 -0400

    fix: Fixed missed service changes from inventory service to item service

--- src/main/frontend/ng-food-inventory/src/components/inventory/out-of-stock-button/out-of-stock-button.component.ts
+++ src/main/frontend/ng-food-inventory/src/components/inventory/out-of-stock-button/out-of-stock-button.component.ts
@@ -1,12 +1,12 @@
 import {Component, Input, inject} from '@angular/core';
 import {Item} from "../../../models/item.model";
-import {InventoryService} from "../../../services";
+import {ItemService} from "../../../services";
 import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-out-of-stock-button',
     template: `
-    <button *ngIf="item" (click)="inventoryService.setOutOfStock(item.id)"
+    <button *ngIf="item" (click)="itemService.setOutOfStock(item.id)"
       class="btn btn-outline btn-circle btn-sm text-secondary-content">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
@@ -17,5 +17,5 @@ import { NgIf } from '@angular/common';
 })
 export class OutOfStockButtonComponent{
   @Input() item: Item | undefined;
-  inventoryService = inject(InventoryService);
+  itemService = inject(ItemService);
 }
