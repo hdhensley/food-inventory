03f8f8c3dd70db728d87fea20bfa91dd12f84516
Daniel Hensley <dhensley@digitalchalk.com>
2026-03-25 11:52:44 -0400

    fix: Updated signal references in templates to resolve the signals from upgrade changes

--- src/main/frontend/ng-food-inventory/src/app/app.component.ts
+++ src/main/frontend/ng-food-inventory/src/app/app.component.ts
@@ -1,26 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { InventoryService } from "../services";
-import { ThemeService } from "../services/theme.service";
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../components/layout';
+import { InventoryService } from '../services';
+import { ThemeService } from '../services/theme.service';
 
-import { ToastViewerComponent } from "../components/layout/toast-viewer/toast-viewer.component";
+import { ToastViewerComponent } from '../components/layout/toast-viewer/toast-viewer.component';
 
 @Component({
-    selector: 'app-root',
-    template: `
-    @if (inventoryService.loaded) {
-      <div
-        class="min-h-screen"
-        [attr.data-theme]="themeService.curTheme()"
-        >
+  selector: 'app-root',
+  template: `
+    @if (inventoryService.loaded()) {
+      <div class="min-h-screen" [attr.data-theme]="themeService.curTheme()">
         <app-navigation></app-navigation>
         <router-outlet></router-outlet>
         <app-toast-viewer></app-toast-viewer>
       </div>
     }
-    `,
-    imports: [NavigationComponent, RouterOutlet, ToastViewerComponent]
+  `,
+  imports: [NavigationComponent, RouterOutlet, ToastViewerComponent],
 })
 export class AppComponent {
   title = 'Food Inventory';
--- src/main/frontend/ng-food-inventory/src/components/inventory/inventory-location-selector/inventory-location-selector.component.ts
+++ src/main/frontend/ng-food-inventory/src/components/inventory/inventory-location-selector/inventory-location-selector.component.ts
@@ -1,18 +1,17 @@
 import { Component, computed, inject } from '@angular/core';
-import { InventoryService } from "../../../services";
+import { InventoryService } from '../../../services';
 import { InventoryFilterComponent } from '../inventory-filter/inventory-filter.component';
 
-
 @Component({
-    selector: 'app-inventory-location-selector',
-    template: `
-      @if (shouldShow) {
-        <div class="tableFilter">
-          <app-inventory-filter></app-inventory-filter>
-        </div>
-      }
-      `,
-    imports: [InventoryFilterComponent]
+  selector: 'app-inventory-location-selector',
+  template: `
+    @if (shouldShow()) {
+      <div class="tableFilter">
+        <app-inventory-filter></app-inventory-filter>
+      </div>
+    }
+  `,
+  imports: [InventoryFilterComponent],
 })
 export class InventoryLocationSelectorComponent {
   shouldShow = computed(() => this.inventoryService.inventory().locations.length > 0);
