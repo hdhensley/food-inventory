465dc8a7520e0c27727f21c2f4b87f8978bb3e98
Daniel Hensley <dhensley@digitalchalk.com>
2026-03-25 12:11:26 -0400

    feat: Upgraded from angular 20 to 21. Built frontend. Fixed linting errors

--- src/main/frontend/ng-food-inventory/src/components/inventory/theme-chooser/theme-chooser.component.ts
+++ src/main/frontend/ng-food-inventory/src/components/inventory/theme-chooser/theme-chooser.component.ts
@@ -1,30 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../../services/theme.service';
 
-
 @Component({
-    selector: 'app-theme-chooser',
-    template: `
+  selector: 'app-theme-chooser',
+  template: `
     <div class="z-50 dropdown dropdown-end">
       <span tabindex="0" class="btn m-1">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-          stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"
+          />
         </svg>
       </span>
-      <ul tabindex="0"
-        class="block dropdown-content menu p-2 shadow bg-neutral text-neutral-content rounded-box w-52 h-60 overflow-y-scroll">
+      <ul
+        tabindex="0"
+        class="block dropdown-content menu p-2 shadow bg-neutral text-neutral-content rounded-box w-52 h-60 overflow-y-scroll"
+      >
         @for (theme of themeService.themes; track theme) {
           <li>
-            <a (click)="themeService.curTheme.set(theme)" (keyup)="themeService.curTheme.set(theme)" tabindex="0">{{ theme }}</a>
+            <a
+              (click)="themeService.curTheme.set(theme)"
+              (keyup)="themeService.curTheme.set(theme)"
+              tabindex="0"
+              >{{ theme }}</a
+            >
           </li>
         }
       </ul>
     </div>
-    `,
-    imports: []
+  `,
+  imports: [],
 })
 export class ThemeChooserComponent {
-  constructor(public themeService: ThemeService) { }
+  themeService = inject(ThemeService);
 }
--- src/main/frontend/ng-food-inventory/src/main.ts
+++ src/main/frontend/ng-food-inventory/src/main.ts
@@ -1,23 +1,28 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 
-
-import { environment } from './environments/environment';
-import { AppComponent } from './app/app.component';
-import { LayoutModule } from './components/layout/layout.module';
-import { PagesModule } from './pages/pages.module';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { ComponentModule } from './components/component.module';
-import { AppRoutingModule } from './app/app-routing.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { ComponentModule } from './components/component.module';
+import { LayoutModule } from './components/layout/layout.module';
+import { environment } from './environments/environment';
+import { PagesModule } from './pages/pages.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, ComponentModule, PagesModule, LayoutModule),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    provideZoneChangeDetection(),
+    importProvidersFrom(
+      BrowserModule,
+      AppRoutingModule,
+      ComponentModule,
+      PagesModule,
+      LayoutModule
+    ),
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch(err => console.error(err));
